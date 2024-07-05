8e4470b142497ce875268066a2553b822fdfdda8
Dragon2000v <shmatko064@gmail.com>
2024-07-05 15:53:24 +0300

    move secret file to .gitignore

--- src/db/initMongoDBConnection.js
+++ src/db/initMongoDBConnection.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
+import { DB_HOST } from '../../config.js';
 
 const initMongoDBConnection = async () => {
   try {
-    const DB_HOST =
-      'mongodb+srv://Dragon2000v:GJtPWoEK43hLZRHQ@dragondata.po0zxba.mongodb.net/my-songs?retryWrites=true&w=majority&appName=DragonData';
     await mongoose.connect(DB_HOST);
     console.log('Successfully connection to MongoDB');
   } catch (error) {
