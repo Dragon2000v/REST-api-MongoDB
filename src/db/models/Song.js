8dc7b42bf57be5fb0b9e9bb93689157f984df70d
Dragon2000v <shmatko064@gmail.com>
2024-07-06 15:12:23 +0300

    add mongoose hooks saveError

--- src/db/models/Song.js
+++ src/db/models/Song.js
@@ -1,15 +1,21 @@
 import { Schema, model } from 'mongoose';
+import { mongoSaveError } from './hooks.js';
 
-const songSchema = new Schema({
-  author: {
-    type: String,
-    required: true,
+const songSchema = new Schema(
+  {
+    author: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
+
+songSchema.post('save', mongoSaveError);
 
 const Song = model('song', songSchema);
 
