c51dee08bb85f57327964ee7e3e73a4b020eacb8
Dragon2000v <shmatko064@gmail.com>
2024-07-06 15:38:05 +0300

    fix fix fix

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,12 +1,12 @@
 import { isValidObjectId } from 'mongoose';
 import HttpError from '../utils/HttpError.js';
 
-const isValidid = (req, res, next) => {
+const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    return next(HttpError(404, `${id} not valid id`));
+    return next(HttpError(404, `${id} is not a valid ID`));
   }
   next();
 };
 
-export default isValidid;
+export default isValidId;
--- src/routers/songs-router.js
+++ src/routers/songs-router.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import songsControllers from '../controllers/songs-controllers.js';
 
-import isValidid from '../middlewares/isValidId.js';
+import isValidId from '../middlewares/isValidId.js';
 
 const songsRouter = Router();
 
 songsRouter.get('/', songsControllers.getSongsController);
-songsRouter.get('/:id', isValidid, songsControllers.getSongByIdController);
+songsRouter.get('/:id', isValidId, songsControllers.getSongByIdController);
 songsRouter.post('/', songsControllers.addSongController);
 export default songsRouter;
